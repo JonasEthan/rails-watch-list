b6d4380baba11f7df2d143398f2cb38dfdb7b864
Jonas Ethan Springer <springerjonasethan@gmail.com>
2022-05-19 16:23:49 +0200

    Implement most methods

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,28 @@
 class BookmarksController < ApplicationController
+  def index
+    redirect_to new_list_bookmark_path(params[:list_id])
+  end
+
+  def new
+    @bookmark = Bookmark.new(list_id: params[:list_id])
+    @list = List.find(params[:list_id])
+    @movies = Movie.all
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save!
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:list_id, :movie_id, :comment)
+  end
 end
